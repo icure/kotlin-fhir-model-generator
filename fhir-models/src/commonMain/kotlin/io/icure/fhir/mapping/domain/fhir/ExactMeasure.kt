ffe3d19ab9cb4c48ef689136c2330a794c8e9a0e
Vincenzo Pierro <vcp@icure.com>
2025-07-14 14:38:08 +0200

    Updated serialization of ExactMeasure

--- fhir-models/src/commonMain/kotlin/io/icure/fhir/mapping/domain/fhir/ExactMeasure.kt
+++ fhir-models/src/commonMain/kotlin/io/icure/fhir/mapping/domain/fhir/ExactMeasure.kt
@@ -3,16 +3,20 @@ package io.icure.fhir.mapping.domain.fhir
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.descriptors.ClassSerialDescriptorBuilder
 import kotlinx.serialization.descriptors.PrimitiveKind
 import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
 import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.descriptors.buildClassSerialDescriptor
+import kotlinx.serialization.descriptors.element
+import kotlinx.serialization.encodeToString
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
+import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonDecoder
 import kotlinx.serialization.json.JsonEncoder
 import kotlinx.serialization.json.JsonUnquotedLiteral
 import kotlinx.serialization.json.jsonPrimitive
-import kotlin.math.pow
 
 @Serializable(ExactMeasureSerializer::class)
 data class ExactMeasure(
@@ -21,7 +25,10 @@ data class ExactMeasure(
 )
 
 class ExactMeasureSerializer : KSerializer<ExactMeasure> {
-	override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("ExactMeasure", PrimitiveKind.STRING)
+	override val descriptor: SerialDescriptor = buildClassSerialDescriptor("ExactMeasure") {
+		element<Int>("value")
+		element<Int>("scale")
+	}
 
 	@OptIn(ExperimentalSerializationApi::class)
 	override fun serialize(
@@ -29,28 +36,29 @@ class ExactMeasureSerializer : KSerializer<ExactMeasure> {
 		value: ExactMeasure
 	) {
 		encoder as JsonEncoder
-		val valueWithPrecision = "${value.value / 10.0.pow(value.scale)}".let {
-			val decimalPart = it.split(".").last()
-			if (value.scale > 0 && decimalPart.length != value.scale) {
-				it.padEnd(it.length + (value.scale - decimalPart.length), '0')
-			} else it
+		val valueToEncode = if (value.scale > 0) {
+			value.value.toString().let {
+				val intPart = it.substring(0, it.length - value.scale)
+				val decimalPart = it.substring(it.length - value.scale)
+				"$intPart.$decimalPart"
+			}
+		} else {
+			value.value.toString().padEnd(value.value.toString().length + value.scale, '0')
 		}
-		encoder.encodeJsonElement(JsonUnquotedLiteral(valueWithPrecision))
+		encoder.encodeJsonElement(JsonUnquotedLiteral(valueToEncode))
 	}
 
 	override fun deserialize(decoder: Decoder): ExactMeasure {
 		decoder as JsonDecoder
 		val content = decoder.decodeJsonElement().jsonPrimitive.content
 		val splitContent = content.split(".")
-		return if (splitContent.size == 1) {
-			ExactMeasure(value = splitContent.first().toInt(), scale = 0)
-		} else {
-			val scale = splitContent.last().length
-			ExactMeasure(
-				value = (content.toDouble() * 10.0.pow(scale)).toInt(),
-				scale = scale
+		return when (splitContent.size) {
+			1 -> ExactMeasure(value = splitContent.first().toInt(), scale = 0)
+			2 -> ExactMeasure(
+				value = "${splitContent.first()}${splitContent.last()}".toInt(),
+				scale = splitContent.last().length
 			)
+			else -> throw IllegalStateException("Invalid float: $content")
 		}
 	}
 }
-
