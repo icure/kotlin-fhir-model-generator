80877d4d0365244d4f582d364354b7a80c61a8be
Vincenzo Pierro <vcp@icure.com>
2025-07-17 15:06:05 +0200

    Small fix on package

--- generator/src/main/kotlin/io/articulus/fhir/generator/FhirStructureDefinitionRenderer.kt
+++ generator/src/main/kotlin/io/articulus/fhir/generator/FhirStructureDefinitionRenderer.kt
@@ -245,12 +245,11 @@ class FhirStructureDefinitionRenderer(private val spec: FhirSpec) {
 
     private fun renderManualValueClasses() {
         Settings.manualValueClasses.forEach { (name, property) ->
-            val packageName = spec.packageName + ".${name.lowercase()}"
-            val out = FileSpec.builder(packageName, name)
+            val out = FileSpec.builder(spec.packageName, name)
             val classBuilder = TypeSpec.classBuilder(name).addModifiers(KModifier.VALUE)
 
             val (propName, typeInfo) = property
-            val className = ClassName(packageName, typeInfo.first)
+            val className = ClassName(spec.packageName, typeInfo.first)
             val propBuilder = PropertySpec.builder(propName, className)
             if (typeInfo.second.isNotBlank()) {
                 propBuilder.initializer(typeInfo.second)
